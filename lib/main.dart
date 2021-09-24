e4676b02192565d47111857787d15f81f033c2d0
Iago Guedelha Nunes <iagognunes@outlook.com>
2021-09-24 08:50:39 -0300

    Gerar PDF + Imprimir

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(
-        title: 'data_table2',
+        title: 'Teste DashBoard',
       ),
     );
   }
